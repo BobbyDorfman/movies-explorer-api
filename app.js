9093e3d5869236c48c364ae2ed7df651459f907c
Sergey Fedorenko <fedorenko_s_a@mail.ru>
2022-06-19 17:30:51 +0300

    feat: Создать схемы и модели ресурсов API

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+// const cors = require('./middlewares/cors');
+const cors = require('cors');
+// const { createUser, login } = require('./controllers/users');
+// const { authorizationValidation, registrationValidation } = require('./middlewares/validation');
+// const { auth } = require('./middlewares/auth');
+// const routes = require('./routes');
+// const serverError = require('./middlewares/errors');
+// const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+// Слушаем 3000 порт
+const { PORT = 3000 } = process.env;
+const app = express();
+app.use(bodyParser.json());
+
+// app.use(cors);
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+  ],
+  credentials: true,
+}));
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+// app.use(requestLogger); // подключаем логгер запросов
+
+// роуты, не требующие авторизации - регистрация и логин
+// app.post('/signup', registrationValidation, createUser);
+// app.post('/signin', authorizationValidation, login);
+
+// авторизация
+// app.use(auth);
+
+// app.use(routes);
+
+// app.use(errorLogger); // подключаем логгер ошибок
+
+app.use(errors());
+// app.use(serverError);
+
+mongoose.connect('mongodb://localhost:27017/moviesdb', () => {
+  // eslint-disable-next-line no-console
+  console.log('Подключение успешно');
+});
+
+app.listen(PORT, () => {
+// Если всё работает, консоль покажет, какой порт приложение слушает
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ models/user.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs');
+const isUrl = require('validator/lib/isURL');
+const isEmail = require('validator/lib/isEmail');
+// const AuthError = require('../errors/AuthError');
+
+const userSchema = new mongoose.Schema({
+  name: { // у пользователя есть имя — опишем требования к имени в схеме:
+    type: String, // имя — это строка
+    default: 'Жак-Ив Кусто', // присвоение стандартного значения
+    minlength: 2, // минимальная длина имени — 2 символа
+    maxlength: 30, // а максимальная — 30 символов
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (email) => isEmail(email),
+      message: 'Неправильный формат почты',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+/*
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new AuthError('Неправильные почта или пароль');
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new AuthError('Неправильные почта или пароль');
+          }
+
+          return user; // теперь user доступен
+        });
+    });
+};
+*/
+
+// создаём модель и экспортируем её
+module.exports = mongoose.model('user', userSchema);
